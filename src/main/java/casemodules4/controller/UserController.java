efe544917a123ccb46ce5f92a51ed14e8fe843e1
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-03-15 14:13:24 +0700

    add file

--- src/main/java/casemodules4/controller/UserController.java
+++ src/main/java/casemodules4/controller/UserController.java
@@ -3,10 +3,15 @@ package casemodules4.controller;
 import casemodules4.model.User;
 import casemodules4.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,6 +21,9 @@ public class UserController {
 
     @Autowired
     private IUserService userService;
+
+    @Value("${upload.path}")
+    private String fileUpload;
 
     @GetMapping
     public ResponseEntity<List<User>> showAllUsers() {
@@ -36,38 +44,49 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> createNewUser(@RequestBody User user){
-        userService.save(user);
-        if (user == null) {
-            new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<User> createNewUser(@RequestBody User user) {
+        MultipartFile multipartFile = user.getImgFile();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(user.getImgFile().getBytes(), new File(fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return new ResponseEntity<>(user,HttpStatus.OK);
+        user.setImgUrl(fileName);
+        userService.save(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user){
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
         User user1 = userService.findById(id);
         if (user1 == null) {
             new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        MultipartFile multipartFile = user.getImgFile();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(user.getImgFile().getBytes(), new File(fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        user.setImgUrl(fileName);
         user.setIdUser(id);
         userService.save(user);
-        return new ResponseEntity<>(user,HttpStatus.OK);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<User> deleteUser(@PathVariable("id") Long id){
+    public ResponseEntity<User> deleteUser(@PathVariable("id") Long id) {
         User user = userService.findById(id);
         userService.deleteById(id);
-        return new ResponseEntity<>(user,HttpStatus.OK);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<User>> findAllUserByName(@RequestParam("nameSearch") String nameSearch){
+    public ResponseEntity<List<User>> findAllUserByName(@RequestParam("nameSearch") String nameSearch) {
         List<User> users = userService.findAllByFullNameContaining(nameSearch);
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
-
-
 
 }
